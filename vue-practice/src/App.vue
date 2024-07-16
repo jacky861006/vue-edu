72e74581b04f07877f687a384788024c7b9a30b6
jacky <jackycheng@uitctech.com.tw>
2024-07-16 15:24:41 +0800

    feat：v-bind掛紅色

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/makeMeRed">MakeMeRed</RouterLink>
       </nav>
     </div>
   </header>
---
+++ vue-practice/src/views/MakeMeRed.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+  <!-- 此處添加一個動態 class 綁定 -->
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<style>
+.title {
+  color: red;
+}
+</style>
